c2485c70d22cedf26545af930f20c54658e88d21
Eric Trexel <etrexel@trexelenterprises.com>
2018-12-02 18:01:25 -0800

    Made main method more manageable for single day runs

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,28 @@ use std::path::Path;
 mod day_one;
 mod day_two;
 
-fn main() {
+fn day_one() {
     let path: &Path = Path::new("input/day_1_part_1.txt");
     println!("Day 1: Part 1");
     day_one::part_one(path);
     println!("Day 1: Part 2");
     day_one::part_two(path);
+}
 
+fn day_two() {
     let path: &Path = Path::new("input/day_2_part_1.txt");
     println!("Day 2: Part 1");
     day_two::part_one(path);
     println!("Day 2: Part 2");
     day_two::part_two(path);
+}
+
+fn main() {
+    let day: i32 = 2;
+
+    match day {
+        1 => day_one(),
+        2 => day_two(),
+        _ => println!("Pick a day!"),
+    };
 }
